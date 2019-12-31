513a6cf001f3c9d1b7bc8f88acd53e80d413b278
Bjørn Jarle Kvande <bjorn.jarle.kvande@gmail.com>
2019-12-31 16:27:27 +0100

    first version

--- source/app.js
+++ source/app.js
@@ -1,1 +1,59 @@
-console.log('App! App! App!');
+window.onload = startApp;
+
+const state = {
+  down: false
+};
+
+function startApp() {
+  const down = e => react(e, { ...state, down: true });
+  const up = e => react(e, { ...state, down: false });
+
+  document.body.addEventListener('touchstart', down);
+  document.body.addEventListener('touchend', down);
+  document.body.addEventListener('mousedown', down);
+  document.body.addEventListener('mouseup', up);
+
+  readSounds();
+
+  render(state);
+}
+
+function react(e, state) {
+  state = { ...state };
+  const { element } = e.target.dataset;
+  if (element === 'button') {
+    render(state);
+    state.down && play();
+  }
+}
+
+function render(state) {
+  const app = document.querySelector('#app');
+  app.innerHTML = html(state);
+}
+
+function html(state) {
+  const button = `app-button ${state.down ? 'app-pressed' : 'app-unpressed'}`;
+  return `
+		<div class="app">
+			 <button data-element="button" class="${button}">App!</button>
+		</div>
+	`;
+}
+
+let sounds = [];
+function readSounds() {
+  try {
+    sounds = [1, 2, 3, 4, 5, 6].map(n => new Audio(`sounds/app${n}.mp3`));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function play() {
+  try {
+    sounds[Math.round(Math.random() * sounds.length)].play();
+  } catch (error) {
+    console.log(error);
+  }
+}
