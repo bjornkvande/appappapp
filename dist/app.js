7c07379c78b44ecfed95afc928ddc604297566d5
Bjørn Jarle Kvande <bjorn.jarle.kvande@gmail.com>
2020-01-01 13:56:52 +0100

    added share button

--- dist/app.js
+++ dist/app.js
@@ -16,6 +16,8 @@ function startApp() {
   document.body.addEventListener('mousedown', down);
   document.body.addEventListener('mouseup', up);
 
+  document.addEventListener('click', shareApp);
+
   render(state);
 }
 
@@ -41,6 +43,11 @@ function html(state) {
       <div class="button-container ${pressed}" style="${playing}">
 		    <button data-element="button" class="app-button">APP</button>
       </div>
+      ${
+  navigator.share
+    ? '<div class="icon share" data-icon="share">' + share + '</div>'
+    : ''
+}
 		</div>
 	`;
 }
@@ -68,6 +75,12 @@ function playAnimation(state) {
     `.trim();
   } else {
     return 'background-color: rgba(0,0,0,0.05);';
+  }
+}
+
+function shareApp(e) {
+  if (e.target.dataset.icon === 'share') {
+    navigator.share('https://appappapp.no');
   }
 }
 
@@ -100,3 +113,9 @@ function background() {
     img;
   return `background-image: url('${src}');`;
 }
+
+const share = `
+  <svg data-icon="share" class="align-middle" viewBox="0 0 24 24">
+    <path data-icon="share" d="M18,16.08C17.24,16.08 16.56,16.38 16.04,16.85L8.91,12.7C8.96,12.47 9,12.24 9,12C9,11.76 8.96,11.53 8.91,11.3L15.96,7.19C16.5,7.69 17.21,8 18,8A3,3 0 0,0 21,5A3,3 0 0,0 18,2A3,3 0 0,0 15,5C15,5.24 15.04,5.47 15.09,5.7L8.04,9.81C7.5,9.31 6.79,9 6,9A3,3 0 0,0 3,12A3,3 0 0,0 6,15C6.79,15 7.5,14.69 8.04,14.19L15.16,18.34C15.11,18.55 15.08,18.77 15.08,19C15.08,20.61 16.39,21.91 18,21.91C19.61,21.91 20.92,20.61 20.92,19A2.92,2.92 0 0,0 18,16.08Z" />
+  </svg>
+`.trim();
