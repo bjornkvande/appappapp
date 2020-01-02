2d9837999ed6e03c99dacb0b843a96e1960ed954
Bjørn Jarle Kvande <bjorn.jarle.kvande@gmail.com>
2020-01-02 10:29:19 +0100

    should not be able to scale app

--- dist/sw.js
+++ dist/sw.js
@@ -16,7 +16,7 @@ if (workbox) {
   },
   {
     "url": "index.html",
-    "revision": "20177a545dfd4a55b613b8445730f056"
+    "revision": "b084e01ac8da5562918688168261312b"
   },
   {
     "url": "manifest.json",
