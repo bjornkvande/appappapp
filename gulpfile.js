ce76cc19a7d02f34658a864f031619496a92e829
Bjørn Jarle Kvande <bjorn.jarle.kvande@gmail.com>
2020-01-05 18:31:56 +0100

    making sure the buildServiceWorker runs last

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,17 @@
-const { src, dest, series, watch } = require('gulp');
+const { src, dest, parallel, series, watch } = require('gulp');
 const mkdirp = require('mkdirp');
 const workboxBuild = require('workbox-build');
 
-// copy the files needed in the top-level directory
-function files(callback) {
+function dir(callback) {
   mkdirp('dist/');
   mkdirp('dist/sounds');
   mkdirp('dist/icons');
-  Promise.all([
-    src('source/app.js').pipe(dest('dist/')),
-    src('source/*.html').pipe(dest('dist/')),
-    src('source/*.json').pipe(dest('dist/')),
-    src('source/*.css').pipe(dest('dist/')),
-    src('sounds/*.*').pipe(dest('dist/sounds/')),
-    src('icons/*.png').pipe(dest('dist/icons/'))
-  ]).then(() => {
-    callback();
-  });
+  callback();
 }
+const source = () => src('source/*.*').pipe(dest('dist/'));
+const sounds = () => src('sounds/*.*').pipe(dest('dist/sounds/'));
+const icons = () => src('sounds/*.*').pipe(dest('dist/sounds/'));
+const files = series(dir, parallel(source, sounds, icons));
 
 // build including service worker
 const build = series(files, buildServiceWorker);
@@ -36,7 +30,6 @@ function buildServiceWorker() {
       swDest: 'dist/sw.js'
     })
     .then(({ count, size, warnings }) => {
-      // Optionally, log any warnings and details.
       warnings.forEach(console.warn);
       console.log(`${count} files will be precached, totaling ${size} bytes.`);
     });
